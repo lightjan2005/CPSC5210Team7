6a2fd3c4ee0bf40993922401e4eb89dbfceb7eb3
Kai Ying Chan <lightjan2005@gmail.com>
2022-04-26 11:44:09 -0700

    Create test case for add_points in test_basketball.py

--- test_basketball.py
+++ test_basketball.py
@@ -1,45 +1,11 @@
 from unittest import TestCase
 
+import basketball
+
 
 class TestBasketball(TestCase):
     def test_add_points(self):
-        self.fail()
+        BasketballObj = basketball.Basketball()
+        BasketballObj.add_points(0, 2)
+        self.assertEqual(2, BasketballObj.score[0])
 
-    def test_ball_passed_back(self):
-        self.fail()
-
-    def test_change_defense(self):
-        self.fail()
-
-    def test_foul_shots(self):
-        self.fail()
-
-    def test_halftime(self):
-        self.fail()
-
-    def test_print_score(self):
-        self.fail()
-
-    def test_start_of_period(self):
-        self.fail()
-
-    def test_two_minute_warning(self):
-        self.fail()
-
-    def test_dartmouth_jump_shot(self):
-        self.fail()
-
-    def test_dartmouth_non_jump_shot(self):
-        self.fail()
-
-    def test_dartmouth_ball(self):
-        self.fail()
-
-    def test_opponent_jumpshot(self):
-        self.fail()
-
-    def test_opponent_non_jumpshot(self):
-        self.fail()
-
-    def test_opponent_ball(self):
-        self.fail()
