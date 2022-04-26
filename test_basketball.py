188751140c3c8f9f4b2f05be9048bc7d13f76130
Kai Ying Chan <lightjan2005@gmail.com>
2022-04-26 10:46:32 -0700

    Create unit test for each method in test_basketball.py

---
+++ test_basketball.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+
+class TestBasketball(TestCase):
+    def test_add_points(self):
+        self.fail()
+
+    def test_ball_passed_back(self):
+        self.fail()
+
+    def test_change_defense(self):
+        self.fail()
+
+    def test_foul_shots(self):
+        self.fail()
+
+    def test_halftime(self):
+        self.fail()
+
+    def test_print_score(self):
+        self.fail()
+
+    def test_start_of_period(self):
+        self.fail()
+
+    def test_two_minute_warning(self):
+        self.fail()
+
+    def test_dartmouth_jump_shot(self):
+        self.fail()
+
+    def test_dartmouth_non_jump_shot(self):
+        self.fail()
+
+    def test_dartmouth_ball(self):
+        self.fail()
+
+    def test_opponent_jumpshot(self):
+        self.fail()
+
+    def test_opponent_non_jumpshot(self):
+        self.fail()
+
+    def test_opponent_ball(self):
+        self.fail()
