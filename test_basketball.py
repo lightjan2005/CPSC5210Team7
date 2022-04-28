469ff0359624ab04fc658a7011f61281f637cf57
Kai Ying Chan <lightjan2005@gmail.com>
2022-04-27 23:55:16 -0700

    add parameterized testing to test_basketball.py

--- test_basketball.py
+++ test_basketball.py
@@ -1,7 +1,10 @@
-from unittest import TestCase
+from unittest import TestCase, mock
+from unittest.mock import Mock
+import unittest
+import pytest
+from parameterized import parameterized
 
 import basketball
-import sys
 
 
 class TestBasketball(TestCase):
@@ -27,16 +30,33 @@ class TestBasketball(TestCase):
         shot = basketball.get_dartmouth_ball_choice(basketballObj.shot_choices)
         self.assertEqual(3, shot)
 
+    # def test_defense_choice_mock(self):
+    #     with mock.patch('builtins.input', return_value=6):
+    #         basketballObj = basketball.Basketball()
+    #         assert basketball.get_defense_choice(basketballObj.defense_choices) == 6
 
-    def test_check_print_score_should_return_true(self):
-        basketballObj = basketball.Basketball()
-        basketballObj.add_points(0, 2)
-        self.assertEqual("Score:  0 to 2", basketballObj.print_score())
+    # @pytest.mark.parametrize('defense_choice', 'result',
+    #                          [
+    #                              (6, 6)
+    #                          ]
+    #                          )
+    # def test_defense_choice_mock(self, defense_choice, result):
+    #     with mock.patch('builtins.input', return_value=defense_choice):
+    #         basketballObj = basketball.Basketball()
+    #         assert basketball.get_defense_choice(basketballObj.defense_choices) == result
+
+    def test_get_defense_choice(self):
+        mock_get_defense_choice = Mock(basketball.get_defense_choice, return_value=6)
+        self.assertEqual(mock_get_defense_choice([]), 6)
 
 
-
-
-
-
-
-
+class TestSequence(unittest.TestCase):
+    @parameterized.expand([
+        [6, 6, ],
+        [7, 7],
+        [6.5, 6.5],
+        [7.5, 7.5]
+    ])
+    def test_sequence(self, a, b):
+        mock_get_defense_choice = Mock(basketball.get_defense_choice, return_value=a)
+        self.assertEqual(mock_get_defense_choice([]), b)
