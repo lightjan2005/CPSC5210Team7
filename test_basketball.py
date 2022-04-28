b0d24fa8d6117dd74a0843d0034d9eceeadcd3fa
Kai Ying Chan <lightjan2005@gmail.com>
2022-04-28 01:56:27 -0700

    create parameterized tests for each test case and rename each test and variables to test_basketball.py

--- test_basketball.py
+++ test_basketball.py
@@ -1,62 +1,57 @@
-from unittest import TestCase, mock
+from basketball import *
+from parameterized import parameterized
 from unittest.mock import Mock
 import unittest
-import pytest
-from parameterized import parameterized
-
-import basketball
 
 
-class TestBasketball(TestCase):
-    def test_add_points(self):
-        BasketballObj = basketball.Basketball()
-        BasketballObj.add_points(0, 2)
-        self.assertEqual(2, BasketballObj.score[0])
+class Test_BasketBall(unittest.TestCase):
+    @parameterized.expand([
+        [0, 2, ],
+        [0, 1],
+        [0, 0],
+        [1, 2],
+        [1, 1],
+        [1, 0]
 
-    def test_defense_choice(self):
-        basketballObj = basketball.Basketball()
-        basketball.print_intro()
-        defense = basketball.get_defense_choice(basketballObj.defense_choices)
-        self.assertEqual(6, defense)
+    ])
+    def test_add_points_return_equal(self, team, score):
+        basketball_obj = Basketball()
+        basketball_obj.add_points(team, score)
+        if team == 0:
+            self.assertEqual(score, basketball_obj.score[0])
+        else:
+            self.assertEqual(score, basketball_obj.score[1])
 
-    def test_opponents_name(self):
-        basketball.print_intro()
-        opponents_name = basketball.get_opponents_name()
-        self.assertEqual("SeattleU", opponents_name)
+    @parameterized.expand([
+        ["Name", "Name"],
 
-    def test_shot_choice(self):
-        basketballObj = basketball.Basketball()
-        basketball.print_intro()
-        shot = basketball.get_dartmouth_ball_choice(basketballObj.shot_choices)
-        self.assertEqual(3, shot)
+    ])
+    def test_shot_choice(self, shot_choice, expected_Value):
+        mock_shot_choice = Mock(get_dartmouth_ball_choice, return_value=shot_choice)
+        self.assertEqual(mock_shot_choice([]), expected_Value)
 
-    # def test_defense_choice_mock(self):
-    #     with mock.patch('builtins.input', return_value=6):
-    #         basketballObj = basketball.Basketball()
-    #         assert basketball.get_defense_choice(basketballObj.defense_choices) == 6
+    @parameterized.expand([
+        [0, 0, ],
+        [1, 1],
+        [2, 2],
+        [3, 3],
+        [4, 4]
+    ])
+    def test_shot_choice(self, shot_choice, expected_Value):
+        mock_shot_choice = Mock(get_dartmouth_ball_choice, return_value=shot_choice)
+        self.assertEqual(mock_shot_choice([]), expected_Value)
 
-    # @pytest.mark.parametrize('defense_choice', 'result',
-    #                          [
-    #                              (6, 6)
-    #                          ]
-    #                          )
-    # def test_defense_choice_mock(self, defense_choice, result):
-    #     with mock.patch('builtins.input', return_value=defense_choice):
-    #         basketballObj = basketball.Basketball()
-    #         assert basketball.get_defense_choice(basketballObj.defense_choices) == result
-
-    def test_get_defense_choice(self):
-        mock_get_defense_choice = Mock(basketball.get_defense_choice, return_value=6)
-        self.assertEqual(mock_get_defense_choice([]), 6)
-
-
-class TestSequence(unittest.TestCase):
     @parameterized.expand([
         [6, 6, ],
         [7, 7],
         [6.5, 6.5],
         [7.5, 7.5]
     ])
-    def test_sequence(self, a, b):
-        mock_get_defense_choice = Mock(basketball.get_defense_choice, return_value=a)
-        self.assertEqual(mock_get_defense_choice([]), b)
+    def test_get_defense_choice(self, defense_choice, expected_Value):
+        mock_get_defense_choice = Mock(get_defense_choice, return_value=defense_choice)
+        self.assertEqual(mock_get_defense_choice([]), expected_Value)
+
+    def test_two_minute_warning(self):
+        basketball_obj = Basketball()
+        basketball_obj.two_minute_warning()
+        self.assertEqual("   *** Two minutes left in the game ***", basketball_obj.two_minute_warning_message)
